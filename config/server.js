58548e85788059b756d0e491a2e8f04a5b393cd6
andnje <andreanjegovan@gmail.com>
2020-11-24 14:00:24 +0100

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15e6b3712a827a5053332b3813288842'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '75f629d9-b32f-414f-b2b4-37d1e611ae41'
+};
