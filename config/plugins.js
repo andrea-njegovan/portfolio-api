3e48563c61ee832d9459ba295c1861da51d194ee
andnje <andreanjegovan@gmail.com>
2020-12-26 21:44:56 +0100

    Blog api created, cloudinary plugin added

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: "dpblbooll",
+      api_key: "146985758556727",
+      api_secret: "_PvGJZfBPcQ0oZ4oAK9wzEcGUDg",
+    },
+  },
+});
